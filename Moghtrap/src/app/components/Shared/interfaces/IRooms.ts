a06d394912d51da4255308a7904e563a09060e17
Mostafa hussien <77110627+mustafa7ussien@users.noreply.github.com>
2022-02-17 03:59:05 +0200

    add room design

--- Moghtrap/src/app/components/Shared/interfaces/IRooms.ts
+++ Moghtrap/src/app/components/Shared/interfaces/IRooms.ts
@@ -2,8 +2,9 @@ export interface IRooms
 {
     //return and types of it from api
     id:number,
-    NumOfBeds:number,
-    Description:string
+    price:number,
+    gender:string,
+    address:string
 
 
 }
